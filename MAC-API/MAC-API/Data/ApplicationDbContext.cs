ade662514f9c4bd50180c0742d0c99c1efe0868c
Slavkata <slavkirilov00@gmail.com>
2019-02-24 21:42:16 +0100

    Adding initial API set up

---
+++ MAC-API/MAC-API/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace MAC_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() :
+          base("DatabaseConnectionString")
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        
+        //add DbSets here
+    }
+}
---
+++ MAC-API/MAC-API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MAC_API.WebApiApplication" Language="C#" %>
