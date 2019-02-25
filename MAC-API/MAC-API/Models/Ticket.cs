98c60c9af618365573d3f5d6e68e113fada08946
Slavkata <slavkirilov00@gmail.com>
2019-02-25 03:40:46 +0100

    Setting up MySql connection and publish packages

---
+++ MAC-API/MAC-API/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MAC_API.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int TicketId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+    }
+}
