336c7a3605503060e92d9762bcfbc12997840c29
Slavkata <slavkirilov00@gmail.com>
2019-04-18 20:40:35 +0200

    Database connection with heroku, tables

--- MAC_API/manage.py
+++ MAC_API/manage.py
@@ -1,5 +1,5 @@
-from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
 
@@ -7,6 +7,7 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
 
 if __name__ == '__main__':
     manager.run()
