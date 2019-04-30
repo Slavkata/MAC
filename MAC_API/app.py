28ee0e653aabc63f39c859fbb26309be6b9387f4
Tuyishime,Aime Thierry A.T <a.thierry@student.fontys.nl>
2019-04-30 17:24:19 +0200

    campingspots and ratings

--- MAC_API/app.py
+++ MAC_API/app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_restful import Api
 from models.db_init import db
 from resources.ticket import TicketResource
+from resources.rating import RatingResource, RatingResourceGet
+from resources.camping import CampingResource, CampingResourceGet
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,5 +17,15 @@ app.config[
     'SQLALCHEMY_DATABASE_URI'] = 'postgres://rhdlffpmcetzoc:29a798c1c1c09f69ce5ec050b9e9e2df40e7376e62b7586c4634773a2accd485@ec2-54-247-70-127.eu-west-1.compute.amazonaws.com:5432/d15pb8k7eqa69t'
 
 api.add_resource(TicketResource,'/ticket')
+
+api.add_resource(UserResource,'/user/register')
+
+#adding the rating
+api.add_resource(RatingResource,'/rating/create')
+api.add_resource(RatingResourceGet,'/ratings/get')
+
+#adding camping
+api.add_resource(CampingResource,'/camping/create')
+api.add_resource(CampingResourceGet,'/campings/get')
 
 db.init_app(app)
--- MAC_API/manage.py
+++ MAC_API/manage.py
@@ -1,12 +1,13 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import db
-from app import app
+
+from app import app, db
 
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
 
 if __name__ == '__main__':
     manager.run()
