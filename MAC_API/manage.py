94b02b2f9ae34f165c7f9ca41445e8462fbc55e9
Tuyishime,Aime Thierry A.T <a.thierry@student.fontys.nl>
2019-04-30 17:24:19 +0200

    campingspots and ratings

--- MAC_API/manage.py
+++ MAC_API/manage.py
@@ -1,7 +1,8 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
-from app import app, db
+from app import app
+from db import db
 
 migrate = Migrate(app, db)
 manager = Manager(app)
