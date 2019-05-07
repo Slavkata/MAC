17196eade5893617142e27444f107ee30031f188
Slavkata <slavkirilov00@gmail.com>
2019-05-07 18:43:12 +0200

    All tables and the migration

--- MAC_API/config.py
+++ MAC_API/config.py
@@ -2,14 +2,11 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config(object):
     DEBUG = True
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-
-
 
 class ProductionConfig(Config):
     DEBUG = False
