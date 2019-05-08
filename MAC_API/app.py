16418a363b8c56b69c8f54eeba339a3b0b9ad9d6
Slavkata <slavkirilov00@gmail.com>
2019-05-08 16:33:21 +0200

    Removing unrelated contents from TICKETS

--- MAC_API/app.py
+++ MAC_API/app.py
@@ -1,31 +1,19 @@
-from	flask import Flask
-from	flask_restful import Api
-from	flask_jwt import JWT
-from  flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_restful import Api
 
-from	security.security import authenticate,identity
-
-from	resources.ticket import TicketResource
-from	resources.user import UserResource
-
-import  os
-
+from resources.ticket import TicketResource
 
 app = Flask(__name__)
 api = Api(app)
 
 app.secret_key = 'blablaabla'
 
-jwt = JWT(app,authenticate,identity)
-
-
 #app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mac.db'
-
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgres://rhdlffpmcetzoc:29a798c1c1c09f69ce5ec050b9e9e2df40e7376e62b7586c4634773a2accd485@ec2-54-247-70-127.eu-west-1.compute.amazonaws.com:5432/d15pb8k7eqa69t'
 
 api.add_resource(TicketResource,'/ticket')
-api.add_resource(UserResource,'/user/register')
 
 if __name__ == '__main__':
     from manage import  db
