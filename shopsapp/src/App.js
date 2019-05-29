4984dc8950f5814e17e1f41a20db4b7d49428919
Boris Mutafov <mutafow@gmail.com>
2019-05-29 20:14:21 +0200

    Load shop request

--- shopsapp/src/App.js
+++ shopsapp/src/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import ShopNumberInput from './components/ShopNumberInput/ShopNumberInput';
+import Axios from 'axios';
+
 
 import './index.css';
 import ProductSelect from './components/ProductSelect/ProductSelect';
 
-function App() {
-  return (
-    <div className="App">
-      <ShopNumberInput />
-      <ProductSelect />
-    </div>
-  );
+Axios.defaults.baseURL = "https://mac-cars.herokuapp.com";
+
+class App extends React.Component {
+
+  state = {
+    id: -1,
+    name: '-',
+  }
+
+  getShopInfo = (e) => {
+    if (e.key !== 'Enter') return;
+    let { value } = e.target;
+    if (!isNaN(value)) {
+      const url = '/shop';
+      Axios.get(url, { params: { id: value } })
+        .then(res => {
+          const { id, name } = res.data;
+          this.setState({ id, name });
+          console.log(res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <ShopNumberInput onInput={this.getShopInfo} shopId={this.state.id} shopName={this.state.name} />
+        {this.state.id !== -1 && <ProductSelect />}
+        {this.state.id === -1 && <span>Please input shop number first</span>}
+      </div>
+    );
+  }
 }
 
 export default App;
--- shopsapp/src/components/ShopNumberInput/ShopNumberInput.js
+++ shopsapp/src/components/ShopNumberInput/ShopNumberInput.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Typography } from '@material-ui/core';
 
 class ShopNumberInput extends React.Component {
   render() {
     return (
-      <input type="text" name="shop-number" className="shop-number" placeholder="Shop number..." />
+      <div className="shop-box">
+        <input type="text" name="shop-number" className="shop-number" placeholder="Shop number..." onKeyDown={this.props.onInput} />
+        <Typography variant="h5">
+          Shop: {this.props.shopName} ({this.props.shopId})
+        </Typography>
+      </div>
     );
   }
 }
