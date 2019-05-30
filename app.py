701a40a02778c93500f0afbed9a61d196701792c
Slavkata <slavkirilov00@gmail.com>
2019-05-30 19:52:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from resources.topup import TopupResource
 
 app = Flask(__name__)
 api = Api(app)
-CORS(app, expose_headers: ['Access-Control-Allow-Origin: *',
+CORS(app, expose_headers= ['Access-Control-Allow-Origin: *',
 'Access-Control-Allow-Methods: GET, POST, PUT, DELETE',
 'Access-Control-Allow-Headers: Authorization'])
 
